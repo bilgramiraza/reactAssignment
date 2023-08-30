4263df65afc5329f55981369d66f8db22d94aaee
bilgramiraza <bilgramiraza@yahoo.com>
2023-08-30 14:45:39 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#React Assignment Project
+
+Built using React, Tailwindcss and Vite
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1 className="text-xl font-bold font-serif"> React Assignment </h1>
+  )
+}
+
+export default App
