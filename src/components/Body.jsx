b6e50fe7bff56aa1f293e27de063cc62ccbf6d77
bilgramiraza <bilgramiraza@yahoo.com>
2023-08-30 15:53:12 +0530

    Added The Required UI Elements

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,7 +1,22 @@
 export default function Body(){
   return (
-    <main className="h-4/6">
-      
+    <main className="flex flex-row h-5/6">
+      <div className="ms-3 ps-3 gird grid-cols-3 gap-3 w-1/4 overflow-scroll border-black border-2">
+        <div className="inline-block m-2 bg-gray-300 w-[100px] h-[100px]">
+        </div>
+        <div className="inline-block m-2 bg-gray-300 w-[100px] h-[100px]">
+        </div>
+        <div className="inline-block m-2 bg-gray-300 w-[100px] h-[100px]">
+        </div>
+        <div className="inline-block m-2 bg-gray-300 w-[100px] h-[100px]">
+        </div>
+        <div className="inline-block m-2 bg-gray-300 w-[100px] h-[100px]">
+        </div>
+      </div>
+      <div className="border-black border-2 w-1/2">
+      </div>
+      <div className="border-black border-2 w-1/4">
+      </div>
     </main>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
 export default function Header(){
   return (
     <header className="bg-gray-900 h-1/6">
-      
+    <div className="h-1/2">
+    </div>
+    <div className="bg-white h-1/2 p-2">
+      <button className="ms-4 text-xl p-2 bg-gray-300">Save</button>
+    </div>
     </header>
   );
 }
